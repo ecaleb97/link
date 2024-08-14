528486317cd4c31b1856b497a8486902dd6b31ee
ecaleb97 <valley_rob.0c@icloud.com>
2024-08-14 15:04:46 +0200

    add fonts

---
+++ src/lib/data.ts
@@ -0,0 +1,32 @@
+import Github from "@/components/icons/github";
+import Linkedin from "@/components/icons/linkedin";
+import X from "@/components/icons/x";
+import { Globe, BookText } from "lucide-react";
+
+export const socialNetworks = [
+	{
+		name: "Portfolio",
+		href: "https://ecaleb.dev",
+		icon: Globe,
+	},
+	{
+		name: "Resume",
+		href: "https://cv.ecaleb.dev",
+		icon: BookText,
+	},
+	{
+		name: "Github",
+		href: "https://github.com/ecaleb97",
+		icon: Github,
+	},
+	{
+		name: "LinkedIn",
+		href: "https://www.linkedin.com/in/caleb-villanueva-zamalloa/",
+		icon: Linkedin,
+	},
+	{
+		name: "X (Twitter)",
+		href: "https://x.com/ecaleb97",
+		icon: X,
+	},
+];
---
+++ src/lib/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Montserrat, Roboto } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const montserrat = Montserrat({ subsets: ["latin"] });
+export const roboto = Roboto({ subsets: ["latin"], weight: ["100", "400"] });
